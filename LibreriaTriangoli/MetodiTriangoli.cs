2ee29b99a6e41fa2f3c3c6c14baf4d43f9c1be49
tommaso lauria <tommaso.lauria9@gmail.com>
2020-11-24 18:07:43 +0100

    Add project files.

---
+++ LibreriaTriangoli/MetodiTriangoli.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace LibreriaTriangoli
+{
+    public class MetodiTriangoli
+    {
+        public static double MisuraLato()
+        {
+            Console.WriteLine("inserisci misura del lato");
+            double l = Convert.ToDouble(Console.ReadLine());
+            return l;
+        }
+        public static string RiconosciTriangoli(double l1, double l2, double l3)
+        {
+            string risultato = "";
+            if (l1 > 0 && l2 > 0 && l3 > 0)
+            {
+                if (l1 == l2 && l1 == l3)
+                {
+                    risultato = "EQUILATERO";
+                }
+                else if (l1 != l2 && l2 != l3 && l3 != l1)
+                {
+                    risultato = "SCALENO";
+                }
+                else
+                {
+                    risultato = "ISOSCELE";
+                }
+            }
+            else
+                risultato = "le misure inserite non sono valide";
+            return risultato;
+        }
+        public static double? Perimetro(double a, double b, double c)
+        {
+            double? perimetro = null;
+            if (a > 0 && b > 0 && c > 0)
+            {
+                perimetro = a + b + c;
+            }
+            else
+            {
+                Console.WriteLine("le misure dei lati inseriti sono errate");
+    
+            }
+            return perimetro;
+        }
+    }
+}
+
+ 
---
+++ TriangoliMetodi/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using LibreriaTriangoli;
+
+namespace TriangoliMetodi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double l1 = MetodiTriangoli.MisuraLato();
+            double l2 = MetodiTriangoli.MisuraLato();
+            double l3 = MetodiTriangoli.MisuraLato();
+            double? perimetro = MetodiTriangoli.Perimetro(l1, l2, l3);
+            string risultato = MetodiTriangoli.RiconosciTriangoli(l1, l2, l3);
+            Console.WriteLine($"IL TRIANGOLO E' {risultato};\nIL PERIMETRO E' {perimetro}.");
+        }
+    }
+}
